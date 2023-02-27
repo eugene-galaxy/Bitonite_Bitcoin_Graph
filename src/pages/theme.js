8406fc794e95d072d8adacbc6267738315635022
Hello world <helloworld288@skiff.com>
2023-02-27 14:58:35 -0500

    orders changed

--- src/pages/theme.js
+++ src/pages/theme.js
@@ -16,26 +16,6 @@ const Page = () => (
         Theme | Carpatin Free
 
             >
-              <Grid
-                xs={12}
-                md={4}
-              >
-                    </Link>
-                    {' '}
-                    as our main font name for all our components.
-                    <br />
-                    <br />
-                    The sizes have been reconfigured to match the needs of a clean
-                    and SEO friendly dashboard.
-                  </Typography>
-                </Stack>
-              </Grid>
-              <Grid
-                xs={12}
-                md={8}
-              >
-                <Card>
-
                 <Typography variant="h6">
                   Body
                 </Typography>
